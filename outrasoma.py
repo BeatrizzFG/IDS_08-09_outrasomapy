84fc68ebce09c9913a028229522a9add17434428
Beatriz França Gusmão <beatrizgusmao421@outlook.com>
2020-09-29 22:00:05 -0300

    Operação com soma, subtração e divisão.

--- outrasoma.py
+++ outrasoma.py
@@ -3,5 +3,7 @@ a = int (input("Primeiro operando:"))
 b = int (input("Segundo operando:"))
 resultado = a + b
 resultadoo = a - b
+resultadooo = a / b
 print (resultado)
 print (resultadoo)
+print (resultadooo)
